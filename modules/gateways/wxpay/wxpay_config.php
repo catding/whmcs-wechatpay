f9b7f9127e4e6129654addda535b24410d113d37
catding <catding@gmail.com>
2019-08-16 18:45:46 +0800

    Update wxpay_config.php

--- modules/gateways/wxpay/wxpay_config.php
+++ modules/gateways/wxpay/wxpay_config.php
@@ -1,13 +1,13 @@
 <?php
 $wxconfig['appid'] = 'wx7cd820fbb8a19f45';
  
-$wxconfig['mch_id'] = '1438497102';
+$wxconfig['mch_id'] = '';
  
-$wxconfig['apikey'] = '9934e7d15453e97507ef794cf7b0519e';
+$wxconfig['apikey'] = '';
  
-$wxconfig['appsecret'] = "b3916a5574bc15888e61ccd988fe7284";
+$wxconfig['appsecret'] = "";
  
-$wxconfig['sslcertPath'] =  'apiclient_cert.pem';
+$wxconfig['sslcertPath'] =  'apiclien.pem';
  
-$wxconfig['sslkeyPath'] = 'apiclient_key.pem';
+$wxconfig['sslkeyPath'] = 'key.pem';
 ?>
